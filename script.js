c99093437a8f49cdf9727e0bf6b7ce610d3baa82
Azirool <aziroolfareed31@gmail.com>
2024-09-24 02:02:46 +0800

    Created the switch case for the rock,scissor, paper game.

--- script.js
+++ script.js
@@ -18,14 +18,59 @@ function getHumanChoice() {
     return userChoice;
 }
 
-function playRound(humanChoice, computerChoice){
-}
-
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+function playGame(humanChoice, computerChoice){
 
-function playGame(){
+   
+    switch(computerChoice) {
+        case "Rock" :
+            switch (humanChoice) {
+                case "Paper" :
+                    console.log("You lose! Rock beats paper");
+                    computerScore++;
+                    break;
+                case "Scissor" :
+                    console.log("You lose! Rock beats Scissor");
+                    computerScore++;
+                    break;
+                default:
+                    console.log("Tie!");
+            }
+            break;
+        case "Paper" :
+            switch (humanChoice) {
+                case "Rock" :
+                    console.log("You win! Rock beats paper");
+                    humanScore++;
+                    break;
+                case "Scissor" :
+                    console.log("You win! Scissor beats paper");
+                    humanScore++;
+                    break;
+                default:
+                    console.log("Tie!");
+            }
+            break;
+        case "Scissor" :
+            switch (humanChoice) {
+                case "Rock" :
+                    console.log("You win! Rock beats scissor");
+                    humanScore++;
+                    break;
+                case "Paper" :
+                    console.log("You lose ! Scissor beats Paper");
+                    computerScore++;
+                    break;
+                default:
+                    console.log("Tie!");
+            }
+            break;
+        default:
+            console.log("Error");
+    }
 
 }
+
+playGame(humanSelection, computerSelection);
